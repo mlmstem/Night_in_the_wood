05403043f64f7e4cc5871bf5ffe3156dbc1ad47b
stefan <stef.temelkovski@gmail.com>
2023-10-04 18:05:14 +1100

    Fixed rain spawner

--- Assets/Scripts/Spawners/SpawnObject.cs
+++ Assets/Scripts/Spawners/SpawnObject.cs
@@ -6,7 +6,7 @@ public class SpawnObject : MonoBehaviour
 {
     [SerializeField] GameObject objectToSpawn;
     private float gameLength = 300.0f; // Spawn for length of game
-    private float MinTime = 1.0f;
+    private float MinTime = 20.0f;
     private float MaxTime = 300.0f;
 
     void Start()
@@ -25,11 +25,10 @@ public class SpawnObject : MonoBehaviour
         // Object spawns in maximum of 3 times in the 5 minutes
         while (Time.time - startTime < gameLength && spawnCount < 3)
         {
-
-            if (GameObject.FindWithTag("Enemy") == null)
+            if (GameObject.FindWithTag("Rain") == null)
             {
                 yield return new WaitForSeconds(Random.Range(MinTime, MaxTime));
-
+                 
                 // Manual Spawn location
                 // Vector3 spawnPosition = new Vector3(-40, 20, 120);
                 // objectInstance = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
