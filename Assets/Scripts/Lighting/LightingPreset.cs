d2ef71288d6f3c8a51cd7c80649db5bafbf51dc3
Stefce02 <100817974+Stefce02@users.noreply.github.com>
2023-10-10 17:02:30 +1100

    Added attribution

--- Assets/Scripts/Lighting/LightingPreset.cs
+++ Assets/Scripts/Lighting/LightingPreset.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+//Modified code from Unity tutorial: https://www.youtube.com/watch?v=m9hj9PdO328
+
 [System.Serializable]
 [CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
 public class LightingPreset : ScriptableObject
