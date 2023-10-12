89c3bc3953faa78a46a1438e78c7446dc3b69276
stefan <stef.temelkovski@gmail.com>
2023-10-12 14:28:09 +1100

    Added sound effect when getting damage and changed damage done by animals

--- Assets/Scripts/Health/HealthManager.cs
+++ Assets/Scripts/Health/HealthManager.cs
@@ -21,6 +21,8 @@ public class HealthManager : MonoBehaviour
     private float reduceHealthMultiplier = 0.7f;
     private bool isInTriggerZone = false;
 
+    public AudioClip damageSound;
+
     // Update is called once per frame
     void Update()
     {
@@ -30,11 +32,42 @@ public class HealthManager : MonoBehaviour
             SceneManager.LoadScene("FailScreen");
         }
 
-        if ((script.distance < 5 && script.counter % 75 == 0 && script.isAttacking) || (script_monkey.distance < 5 && script_monkey.counter % 75 == 0 && script_monkey.isAttacking) || (script_deer.distance < 5 && script_deer.counter % 75 == 0 && script_deer.isAttacking) || (script_snake.distance < 5 && script_snake.counter % 75 == 0 && script_snake.isAttacking) || (script_lizard.distance < 5 && script_lizard.counter % 75 == 0 && script_lizard.isAttacking))
+        // Bear damage
+        if (script.distance < 5 && script.counter % 75 == 0 && script.isAttacking)
         {
-            Debug.Log("Take Damage");
             TakeDamage(25);
+            GetComponent<AudioSource>().clip = damageSound;
+            GetComponent<AudioSource>().Play();
         }
+        // Monkey damage
+        else if (script_monkey.distance < 5 && script_monkey.counter % 75 == 0 && script_monkey.isAttacking)
+        {
+            TakeDamage(15);
+            GetComponent<AudioSource>().clip = damageSound;
+            GetComponent<AudioSource>().Play();
+        }
+        // Deer damage
+        else if (script_deer.distance < 5 && script_deer.counter % 75 == 0 && script_deer.isAttacking)
+        {
+            TakeDamage(15);
+            GetComponent<AudioSource>().clip = damageSound;
+            GetComponent<AudioSource>().Play();
+        }
+        // Snake damage
+        else if (script_snake.distance < 5 && script_snake.counter % 75 == 0 && script_snake.isAttacking)
+        {
+            TakeDamage(20);
+            GetComponent<AudioSource>().clip = damageSound;
+            GetComponent<AudioSource>().Play();
+        }
+        // Lizard damage
+        else if (script_lizard.distance < 5 && script_lizard.counter % 75 == 0 && script_lizard.isAttacking)
+        {
+            TakeDamage(10);
+            GetComponent<AudioSource>().clip = damageSound;
+            GetComponent<AudioSource>().Play();
+        }
+
 
         if (isInTriggerZone && GameObject.Find("Rain(Clone)") != null)
         {
