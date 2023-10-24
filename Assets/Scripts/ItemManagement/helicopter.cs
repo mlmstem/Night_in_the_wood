b853754473ab92f9f5a0510fa8c119b62ff0fea2
volo10 <102479263+volo10@users.noreply.github.com>
2023-10-24 08:39:02 +0300

    bug fixes

--- Assets/Scripts/ItemManagement/helicopter.cs
+++ Assets/Scripts/ItemManagement/helicopter.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class helicopter : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float fallSpeed = 0.01f;
     void Start()
     { // Use Start or Awake depending on your goal
         gameObject.active = false;
-        Invoke("wakeup", 290f);  // Time in seconds
+        Invoke("wakeup", 3f);  // Time in seconds
     }
 
     void wakeup()
     {
         gameObject.active = true;
     }
-    
+    private void Update()
+    {
+        // Move the helicopter downwards in the Y-axis
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+    }
+
 }
