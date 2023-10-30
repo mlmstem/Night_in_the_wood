4a3ebe0496d1499f6b80f14389563db6fe5e9699
stefan <stef.temelkovski@gmail.com>
2023-10-30 15:42:33 +1100

    Fixed pickup when already inside trigger area

--- Assets/Scripts/ItemManagement/PickupItem.cs
+++ Assets/Scripts/ItemManagement/PickupItem.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Modified code from unity tutorial: https://www.youtube.com/watch?v=DLAIYSMYy2g
-
 public class PickupItem : MonoBehaviour
 {
-
     private PlayerHotbar hotbar;
     public GameObject itemButton;
-
     public AudioSource pickupSound;
+
+    private bool canPickup = false;
+
     void Start()
     {
         hotbar = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHotbar>();
@@ -20,23 +19,43 @@ public class PickupItem : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            for (int i = 0; i < hotbar.slots.Length; i++)
-            {
-                if (hotbar.isFull[i] == false)
-                {
-                    Debug.Log("itemAdded");
-                    // Item can be added to hotbar
-                    if (pickupSound != null)
-                    {
-                        pickupSound.Play();
-                    }
-                    hotbar.isFull[i] = true;
-                    Instantiate(itemButton, hotbar.slots[i].transform, false);
-                    Destroy(transform.parent.gameObject);
-                    break;
-                }
-            }
+            canPickup = true;
         }
     }
 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canPickup = false;
+        }
+    }
+
+    void Update()
+    {
+        if (canPickup)
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        for (int i = 0; i < hotbar.slots.Length; i++)
+        {
+            if (hotbar.isFull[i] == false)
+            {
+                Debug.Log("itemAdded");
+                // Item can be added to hotbar
+                if (pickupSound != null)
+                {
+                    pickupSound.Play();
+                }
+                hotbar.isFull[i] = true;
+                Instantiate(itemButton, hotbar.slots[i].transform, false);
+                Destroy(transform.parent.gameObject);
+                break;
+            }
+        }
+    }
 }
