c5c18bd3849b90ab343a8e63d1a207333cbc53db
Stefce02 <100817974+Stefce02@users.noreply.github.com>
2023-10-31 09:22:48 +1100

    Reverted pickup item changes

--- Assets/Scripts/ItemManagement/PickupItem.cs
+++ Assets/Scripts/ItemManagement/PickupItem.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Modified code from unity tutorial: https://www.youtube.com/watch?v=DLAIYSMYy2g
+
 public class PickupItem : MonoBehaviour
 {
+
     private PlayerHotbar hotbar;
     public GameObject itemButton;
+
     public AudioSource pickupSound;
-
-    private bool canPickup = false;
-
     void Start()
     {
         hotbar = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHotbar>();
@@ -19,43 +20,23 @@ public class PickupItem : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            canPickup = true;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            canPickup = false;
-        }
-    }
-
-    void Update()
-    {
-        if (canPickup)
-        {
-            Pickup();
-        }
-    }
-
-    void Pickup()
-    {
-        for (int i = 0; i < hotbar.slots.Length; i++)
-        {
-            if (hotbar.isFull[i] == false)
+            for (int i = 0; i < hotbar.slots.Length; i++)
             {
-                Debug.Log("itemAdded");
-                // Item can be added to hotbar
-                if (pickupSound != null)
+                if (hotbar.isFull[i] == false)
                 {
-                    pickupSound.Play();
+                    Debug.Log("itemAdded");
+                    // Item can be added to hotbar
+                    if (pickupSound != null)
+                    {
+                        pickupSound.Play();
+                    }
+                    hotbar.isFull[i] = true;
+                    Instantiate(itemButton, hotbar.slots[i].transform, false);
+                    Destroy(transform.parent.gameObject);
+                    break;
                 }
-                hotbar.isFull[i] = true;
-                Instantiate(itemButton, hotbar.slots[i].transform, false);
-                Destroy(transform.parent.gameObject);
-                break;
             }
         }
     }
+
 }
